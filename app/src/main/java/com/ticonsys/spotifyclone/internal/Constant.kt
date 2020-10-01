fb1ee6e205dcfaefcbe9f968cfb5d360258d5bce
android-tms <jewel@ticonsys.com>
2020-10-02 01:06:16 +0600

    complete service implemenation

--- app/src/main/java/com/ticonsys/spotifyclone/internal/Constant.kt
+++ app/src/main/java/com/ticonsys/spotifyclone/internal/Constant.kt
@@ -3,4 +3,11 @@ package com.ticonsys.spotifyclone.internal
 object Constant {
 
     const val SONG_COLLECTION = "songs"
+
+    const val NOTIFICATION_ID = 1
+    const val NOTIFICATION_CHANNEL_ID = "music"
+
+
+    const val MEDIA_ROOT_ID = "root_id"
+
 }
