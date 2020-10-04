d0e60414ce55076bd899e66a4000a58b60dd8153
Jewel Rana <jewel@ticonsys.com>
2020-10-04 11:28:36 +0600

    resource and event implemenation

---
+++ app/src/main/java/com/ticonsys/spotifyclone/internal/Event.kt
@@ -0,0 +1,18 @@
+package com.ticonsys.spotifyclone.internal
+
+open class Event<out T>(private val data: T) {
+
+    var hasBeenHandled = false
+        private set
+
+    fun getContentIfNotHandled(): T? {
+        return if(hasBeenHandled){
+            return null
+        } else{
+            hasBeenHandled = true
+            data
+        }
+    }
+
+    fun peekContent() = data
+}
